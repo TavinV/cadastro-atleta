d1ff12a886fb81d058a32f97d4080f0de60bc135
Otávio Vinícius Flauzino <159206555+TavinV@users.noreply.github.com>
2024-08-19 00:35:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let nome_atleta = document.getElementById("nome-do-atleta")
+let rg = document.getElementById("rg")
+let cpf = document.getElementById("cpf")
+let data_nascimento = document.getElementById("data-nascimento")
+let telefone = document.getElementById("telefone")
+let posicao = document.getElementById("posicao")
+let altura = document.getElementById("altura")
+let experiencia = document.getElementById("experiencia")
+
+let form = document.querySelector("form")
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    atleta = {
+        nome: nome_atleta.value.trim(),
+        rg: rg.value.trim(),
+        cpf: cpf.value.trim(),
+        data_nascimento: data_nascimento.value.trim(),
+        tel: telefone.value.trim(),
+        posicao: posicao.value.trim(),
+        altura: altura.value.trim(),
+        experiencia: experiencia.value.trim()
+    }
+
+    let jsonAtleta = JSON.stringify(atleta)
+
+    console.log(jsonAtleta)
+})
